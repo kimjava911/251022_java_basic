399e2a320b2acc99c87806fa351ebd61bff9db0f
kimjava911 <aibackend4th@gmail.com>
2025-10-22 10:12:17 +0900

    Initial commit

---
+++ src/Example01.java
@@ -0,0 +1,15 @@
+// ctrl + /, cmd + / -> 주석 (//)
+// JS -> window. -> 별도로 묶어서 시작할 필요는 없었음
+// Java -> Class. (Java 8을 중심으로 설명)
+// java -> class 파일로 컴파일을 한다... IntelliJ 환경에서 의식할 필요는 없음
+// '컴파일'한다는 건 중요 -> 실행하기 앞서서 검증작업을 한다
+public class Example01 { // class -> object 객체.
+    // -> public (접근제어자)
+    // class -> 사용.
+    // 변수와 함수. -> 변수 => class 내부에 들어가면 '멤버변수', '속성', '필드'
+    // 함수 -> 메서드(method)
+
+    // 주석 -> 한줄주석(//) -> ctrl + /, cmd + /
+    // 여러줄주석(/* */) -> ctrl + shift + /, cmd + shift + /
+    
+}
